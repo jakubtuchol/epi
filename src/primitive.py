cf98fb7c380ad7621d32ee3b52573d9b61880ab4
Jakub Tucholski <jakub.tucholski@houzz.com>
2016-07-19 14:58:49 -0700

    workin on integer reversal

--- src/primitive.py
+++ src/primitive.py
@@ -1,3 +1,4 @@
+from math import ceil
 def find_parity(x):
     parity = 0
 
@@ -7,8 +8,24 @@ def find_parity(x):
 
     return parity % 2
 
-def reverse_digits():
+def reverse_digits(num):
     '''
     Problem 5.8
     '''
-    pass
+    negative = False
+    if num < 0:
+        negative = True
+        num *= -1
+
+    reversal = list(str(num))
+    upper = len(reversal) - 1
+    max_loc = int(ceil(len(reversal) / 2))
+    for idx in xrange(max_loc):
+        tmp = reversal[idx]
+        reversal[idx] = reversal[upper-idx]
+        reversal[upper-idx] = tmp
+
+    rev_num = int(''.join(reversal))
+    if negative:
+        return -1 * rev_num
+    return rev_num
--- test/test_primitive.py
+++ test/test_primitive.py
@@ -1,4 +1,4 @@
-from src.primitive import find_parity
+from src.primitive import find_parity, reverse_digits
 
 def test_basic_parity():
     x = int('1011', 2)
@@ -7,3 +7,9 @@ def test_basic_parity():
 def test_basic_nonparity():
     x = int('10001000', 2)
     assert find_parity(x) == 0
+
+def test_basic_reverse():
+    assert reverse_digits(42) == 24
+
+def test_negative_reverse():
+    assert reverse_digits(-314) == -413
