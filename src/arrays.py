fec38a21f8123e323664d1d8a8f347b3b3bef1e3
Jakub Tucholski <jakub.tucholski@houzz.com>
2016-07-19 20:51:28 -0700

    tests still kinda failing

--- src/arrays.py
+++ src/arrays.py
@@ -9,4 +9,25 @@ def dutch_national_partition(idx, arr):
     to arr[idx], then all elements greater than
     arr[idx]
     '''
-    pass
+    def swap(arr, idx, idy):
+        tmp = arr[idx]
+        arr[idx] = arr[idy]
+        arr[idy] = tmp
+
+    pivot = arr[idx]
+
+    smaller = 0
+    for idy, elt in enumerate(arr):
+        # group all elements less than pivot at
+        # the bottom
+        if elt < pivot:
+            swap(arr, smaller, idy)
+            smaller += 1
+
+    larger = len(arr) - 1
+    for idy, elt in enumerate(arr):
+        # group all elements greater than pivot
+        # at the bottom
+        if elt > pivot:
+            swap(arr, larger, idy)
+            larger -= 1
--- test/test_arrays.py
+++ test/test_arrays.py
@@ -2,7 +2,41 @@ from src.arrays import dutch_national_partition
 
 class TestDutchNationalFlag:
     def test_arr_ordered(self):
+        '''
+        should pass automatically, array
+        is already ordered
+        '''
         arr = [1,2,3,4,5,6,7,8,9,10]
         dutch_national_partition(5, arr)
-        for x in range(1,11):
-            assert arr[x-1] == x
+        for x in xrange(1,11):
+            if x < 5:
+                assert arr[x-1] < 5
+            else:
+                assert arr[x-1] >= 5
+
+    def test_arry_reversed(self):
+        '''
+        should properly reverse array
+        '''
+        arr = [10,9,8,7,6,5,4,3,2,1]
+        dutch_national_partition(5, arr)
+        for x in xrange(1,11):
+            if x < 5:
+                assert arr[x-1] < 5
+            else:
+                assert arr[x-1] >= 5
+
+    def test_repeated_elements(self):
+        '''
+        should function properly on
+        an array with repeating elements
+        '''
+        arr = [5,3,1,5,5,3,3,1,1]
+        dutch_national_partition(1, arr)
+        for x in xrange(9):
+            if x <= 2:
+                assert arr[x] == 1
+            elif x <= 5:
+                assert arr[x] == 3
+            else:
+                assert arr[x] == 5
