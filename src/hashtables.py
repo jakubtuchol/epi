cc403fb7272fa1ed7d522d7a741106ae98b4f3b1
Jakub Tucholski <jtucholski00@gmail.com>
2016-08-30 23:51:40 -0700

    working on hashtables

--- src/hashtables.py
+++ src/hashtables.py
@@ -13,12 +13,32 @@ def find_anagrams(words):
     return [val for val in anagrams.values() if len(val) > 1]
 
 @attr.s
+class Node(object):
+    _val = attr.ib(validator=instance_of(str))
+    _prev = attr.ib()
+    _next = attr.ib()
+
+@attr.s
 class IsbnCache(object):
     _capacity = attr.ib(validator=instance_of(int))
-    _contents = attr.ib(default=attr.Factory(dict)) 
+    _contents = attr.ib(default=attr.Factory(dict))
+    _head_node = None
+    _tail_node = None
+    _cur_size = 0
 
     def insert(self, isbn):
-        pass
+        if self._cur_size == self._capacity:
+            # need to least recently accessed node
+            del self._contents[self._tail_node._val]
+            self._tail_node = self._tail_node._prev
+            self._tail_node._next = None
+        new_node = Node(
+            val=isbn,
+            prev=None,
+            next=self._head_node,
+        )
+        self._head_node = new_node
+        self._contents[isbn] = new_node
 
     def lookup(self, isbn):
         pass
