f989a627e9a803c6ec80e9d2e31f4ea9eb91c734
Jakub Tucholski <jtucholski00@gmail.com>
2016-09-01 18:02:45 -0700

    adding failing test

--- test/test_hashtables.py
+++ test/test_hashtables.py
@@ -1,4 +1,4 @@
-from src.hashtables import find_anagrams
+from src.hashtables import find_anagrams, IsbnCache
 
 class TestFindAnagrams:
     '''
@@ -45,4 +45,27 @@ class TestIsbnCache:
     Question
     '''
     def test_serial_insert_removes_least_recent(self):
-        self
+        isbns = [
+            ('1101874937',16.51),
+            ('0393245446',11.71),
+            ('1455561789',13.99),
+            ('0062414216',17.50),
+            ('081298840X',15.00),
+            ('0553447432',16.71),
+            ('081299860X',16.20),
+            ('0812997743',18.36),
+            ('1616204583',17.65),
+            ('1610395832',14.93),
+        ]
+        cache = IsbnCache(capacity=5)
+
+        # insert first five elements
+        for isbn in isbns[:5]:
+            cache.insert(isbn[0],isbn[1])
+
+        # check that each of these are in cache
+        for isbn in isbns[:5]:
+            assert isbn[0] in cache._contents
+            assert cache._tail_node == cache._contents[isbn[0]]
+            assert isbn[1] == cache.lookup(isbn)
+            assert isbn._head_node == cache._contents[isbn[0]]
