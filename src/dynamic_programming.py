a1deec49bb018926a3c8d7e80a060cf891846fe8
Jakub Tucholski <jtucholski00@gmail.com>
2016-09-06 10:15:32 -0700

    completing dynamic programming

--- src/dynamic_programming.py
+++ src/dynamic_programming.py
@@ -8,19 +8,12 @@ def count_score_combinations(score):
     this score
     '''
     ways_to_score = [2,3,7]
-    scores = [1] + ([0] * score)
+    scores = ([[1] + ([0] * score)]) * len(ways_to_score)
 
-    for idx in xrange(1, score + 1):
-        print('handling score {}'.format(idx))
-        min_seven = idx - 7
-        min_three = idx - 3
-        min_two = idx - 2
-        score_seven = 0 if min_seven < 0 else scores[min_seven]
-        print('min_seven is {}, score_seven is {}'.format(min_seven, score_seven))
-        score_three = 0 if min_three < 0 else scores[min_three]
-        print('min_three is {}, score_three is {}'.format(min_three, score_three))
-        score_two = 0 if min_two < 0 else scores[min_two]
-        print('min_two is {}, score_two is {}'.format(min_two, score_two))
-        scores[idx] = score_seven + score_three + score_two
-    print('scores are {}'.format(scores))
-    return scores[-1]
+    for play_idx in xrange(len(ways_to_score)):
+        for idx in xrange(1, score + 1):
+            without_play = scores[play_idx-1][idx] if play_idx >= 1 else 0
+            with_play = scores[play_idx][idx-ways_to_score[play_idx]] if \
+                    idx >= ways_to_score[play_idx] else 0
+            scores[play_idx][idx] = without_play + with_play
+    return scores[-1][-1]
